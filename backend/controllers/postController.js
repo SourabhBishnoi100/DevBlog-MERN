4f61320eac5f1d43bfdabb2af8c5490ff9e6675f
Sourabh Bishnoi <129992874+SourabhBishnoi100@users.noreply.github.com>
2026-05-12 17:18:16 +0530

    feat(backend): Add getPostById controller function and route, add pagation to getPosts function, remove postcontroller.js file (wrongly named)

---
+++ backend/controllers/postController.js
@@ -0,0 +1,116 @@
+import Post from "../models/Post.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+export const getPosts = asyncHandler(async (req, res, next) => {
+    
+    const page = req.query.page || 1;
+
+    const limit = 10;
+
+    const skip = (page - 1) * limit;
+
+    const totalPosts = await Post.countDocuments();
+
+    const posts = await Post.find().select("title slug excerpt coverImage author tags").populate("author", "name").sort({ createdAt: -1 }).skip(skip).limit(limit);
+
+    
+    return res.status(200).json({
+        success: true,
+        currentPage : page,
+        totalPages : Math.ceil(totalPosts/limit),
+        totalPosts,
+        count: posts.length,
+        data: posts,
+    });
+})
+
+export const getPostById = asyncHandler( async (req, res, next) => {
+    const postId = req.params.id;
+
+    const post = await Post.findById(postId).populate("author", "name email");
+
+    if(!post){
+        res.status(404);
+        throw new Error("Post not found");
+    }
+
+    res.status(200).json({
+        success: true,
+        data: post
+    })
+})
+
+export const createPost = asyncHandler(async (req, res, next) => {
+    const newPost = await Post.create({ ...req.body, author: req.user._id });
+
+    return res.status(201).json({
+        success: true,
+        data: newPost
+    })
+})
+
+export const deletePost = asyncHandler(async (req, res, next) => {
+
+    const post = await Post.findById(req.params.id);
+
+    if(!post) {
+        res.status(404);
+        throw new Error("Post not found");
+    }
+
+    if(post.author.toString() !== req.user._id.toString()){
+        res.status(401);
+        throw new Error("not authorized, couldn't delete the post !");
+    } 
+
+    await post.deleteOne();
+
+    return res.status(200).json({ 
+        success: true,
+        message: "Post deleted successfully...." 
+    });
+})
+
+export const getPostBySlug = asyncHandler(async (req, res, next) => {
+    const post = await Post.findOne({ slug: req.params.slug });
+    if (!post) {
+        return res.status(404).json({ message: "Post not found..." });
+    }
+
+    return res.status(200).json({
+        sucess: true,
+        data: post
+    });
+})
+
+export const updatePost = asyncHandler(async (req, res, next) => {
+
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+        res.status(404);
+        throw new Error("Post not found !");
+    }
+
+    if (post.author.toString() !== req.user._id.toString()) {
+        res.status(401);
+        throw new Error("Not authorized !");
+    }
+
+    post.title = req.body.title || post.title;
+    post.slug = req.body.slug || post.slug;
+    post.excerpt = req.body.excerpt || post.excerpt;
+    post.coverImage = req.body.coverImage || post.coverImage;
+    post.content = req.body.content || post.content;
+    // post.author = req.body.author || post.author;
+    post.tags = req.body.tags || post.tags;
+
+
+    const updatedPost = await post.save();
+
+    return res.status(200).json({
+        success: true,
+        data: updatedPost
+    });
+});
+
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createPost, deletePost, getPostBySlug, getPosts, updatePost } from "../controllers/postController.js";
+import { createPost, deletePost, getPostById, getPostBySlug, getPosts, updatePost } from "../controllers/postController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/",protect, createPost);
 router.get("/", getPosts);
+router.get("/:id", getPostById);
 router.get("/:slug", getPostBySlug);
 router.put("/:id",protect, updatePost);
 router.delete("/:id",protect, deletePost);
